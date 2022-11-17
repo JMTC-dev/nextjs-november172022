795ec31c2f9b4a1588fff44e81f8aaeb3fb98e46
Jack Casey <jackmatthewcasey@gmail.com>
2022-11-17 12:14:56 +1000

    feat: create CatCard

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: 'Felines',
+  title: 'Cats',
+  body: 'Smudge',
+  author: 'Me',
+  time: '1s ago',
+};
+
+export const mockCatCardProps = {
+  base,
+};
